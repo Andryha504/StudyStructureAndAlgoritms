5380aa4b217d98289e9f3bce24d8e407f7def749
Andriy Gromysh <andryha504@i.ua>
2020-02-22 22:15:53 +0200

    added buble sorting

---
+++ StudyStructureAndAlgoritms/StudyStructureAndAlgoritms.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mas[5];
+    int size = 5;
+    int k = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        mas[i] = rand();
+        cout << mas[i] << "\t";
+    }
+    for (int a = 1; a < size; a++)
+    {
+        for (int b=size-1; b >= a; b--)
+        {
+            if (mas[b - 1] > mas[b])
+            {
+                k = mas[b - 1];
+                mas[b - 1] = mas[b];
+                mas[b] = k;
+            }
+     }
+    }
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << mas[i] << "\t";
+    }
+
+
+
+}
